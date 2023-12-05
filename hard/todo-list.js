6f7f03ddcf1915f5f9fcdc1eb8f471b2bd12d81c
Vikas Swain <vikas@Vikass-MacBook-Air.local>
2023-12-05 14:23:26 +0530

    hard files added

---
+++ hard/todo-list.js
@@ -0,0 +1,50 @@
+/*
+  Implement a class `Todo` having below methods
+    - add(todo): adds todo to list of todos
+    - remove(indexOfTodo): remove todo from list of todos
+    - update(index, updatedTodo): update todo at given index
+    - getAll: returns all todos
+    - get(indexOfTodo): returns todo at given index
+    - clear: deletes all todos
+
+  Once you've implemented the logic, test your code by running
+*/
+
+class Todo {
+
+  constructor(){
+    this.todos = [];
+  }
+
+  add(todo){
+    this.todos.push(todo);
+  }
+
+  getAll(){
+    return this.todos;
+  }
+
+  remove(indexOfTodo){
+    if(indexOfTodo > this.todos.length)
+      return null;
+    this.todos.splice(indexOfTodo,1);
+  }
+
+  update(index, updatedTodo){
+    if (index >=this.todos.length) 
+      return null;
+    this.todos[index]=updatedTodo;
+  }
+
+  get(indexOfTodo){
+    if(indexOfTodo >= this.todos.length)
+      return null;
+    return this.todos[indexOfTodo];
+  }
+
+  clear(){
+    this.todos = [];
+  }
+}
+
+module.exports = Todo;
